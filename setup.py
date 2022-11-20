3236bf6f4c813d18e4034b6d917445f0a2dd8eae
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2022-11-19 19:25:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,23 @@
-import setuptools
+from setuptools import setup
+ 
+setup(
+    name='studentresume',
+    version='0.1.0',    
+    description='Student Resume Generator Python package',
+    url='https://github.com/open-uofa/studentresume',
+    author='Diana',
+    author_email='dble@ualberta.ca',
+    license='MIT License',
+    packages=['studentresume'],
+    install_requires=[
+                    'jsonschema==4.16.0',
+                    'reportlab==3.6.11',         
+                    ],
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='example_pypi_package',
-    author='Tom Chen',
-    author_email='tomchen.org@gmail.com',
-    description='Example PyPI (Python Package Index) Package',
-    keywords='example, pypi, package',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/tomchen/example_pypi_package',
-    project_urls={
-        'Documentation': 'https://github.com/tomchen/example_pypi_package',
-        'Bug Reports':
-        'https://github.com/tomchen/example_pypi_package/issues',
-        'Source Code': 'https://github.com/tomchen/example_pypi_package',
-        # 'Funding': '',
-        # 'Say Thanks!': '',
-    },
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
     classifiers=[
-        # see https://pypi.org/classifiers/
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10',
     ],
-    python_requires='>=3.6',
-    # install_requires=['Pillow'],
-    extras_require={
-        'dev': ['check-manifest'],
-        # 'test': ['coverage'],
-    },
-    # entry_points={
-    #     'console_scripts': [  # This can provide executable scripts
-    #         'run=examplepy:main',
-    # You can execute `run` in bash to run `main()` in src/examplepy/__init__.py
-    #     ],
-    # },
 )
