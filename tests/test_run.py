f74ba92af3b4ac84eb3f2d4f4d56c74afd174482
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2022-12-03 13:56:37 -0700

    updated tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,13 +2,15 @@ from typer.testing import CliRunner
 import os.path
 
 default_theme_path = os.path.join("studentresume", "themes", "default.json")
+sample_resume_path = os.path.join("studentresume", "sample.resume.json")
+one_page_resume = os.path.join("studentresume", "testonepage.resume.json")
 
 from studentresume.run import app
 
 runner = CliRunner()
 
 def test_app_basic():
-    result = runner.invoke(app, ["sample.resume.json"])
+    result = runner.invoke(app, [sample_resume_path])
     assert result.exit_code == 0
     assert "Valid JSON" in result.stdout
     assert "Generating resume with sample.resume.json and style modern" in result.stdout
@@ -17,16 +19,16 @@ def test_app_basic():
     assert "Invalid value: Requires a resume JSON file as an argument" in result.stdout
     
 def test_app_theme():
-    result = runner.invoke(app, ["sample.resume.json", "--theme", "centered"])
+    result = runner.invoke(app, [sample_resume_path, "--theme", "centered"])
     assert result.exit_code == 0
     assert "Valid JSON" in result.stdout
     assert "Generating resume with sample.resume.json and style centered" in result.stdout
-    result = runner.invoke(app, ["sample.resume.json", "--theme", "classic"])
+    result = runner.invoke(app, [sample_resume_path, "--theme", "classic"])
     assert result.exit_code == 2
     assert "Invalid value for '--theme' / '-t': classic is not a supported default theme" in result.stdout
     
 def test_app_page():
-    result = runner.invoke(app, ["testonepage.resume.json", "--onepage"])
+    result = runner.invoke(app, [one_page_resume, "--onepage"])
     assert result.exit_code == 0
     assert "Valid JSON" in result.stdout
     assert "restricting to one page" in result.stdout
@@ -38,14 +40,12 @@ def test_app_invalid_json():
     assert "Invalid value: Resume JSON file not found" in result.stdout
     
 def test_app_invalid_theme():
-    result = runner.invoke(app, ["sample.resume.json", "invalid.theme.json"])
+    result = runner.invoke(app, [sample_resume_path, "invalid.theme.json"])
     assert result.exit_code == 2
     assert "Invalid value: Theme JSON file not found" in result.stdout
     
 def test_app_valid_theme():
-    theme = str(os.path.join("themes", "default.json"))
-    print(default_theme_path)
-    result = runner.invoke(app, ["sample.resume.json", default_theme_path])
+    result = runner.invoke(app, [sample_resume_path, default_theme_path])
     assert result.exit_code == 0
     assert "Valid JSON" in result.stdout
     assert "Generating resume with sample.resume.json and style custom theme!" in result.stdout
